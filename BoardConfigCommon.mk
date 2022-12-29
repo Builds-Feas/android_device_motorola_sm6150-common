a1a466071aa565ab07b22c2f2ac4e0116b2262c7
ViShal69x <tk5762996@gmail.com>
2022-12-29 11:37:06 +0530

    sm6150-common: Switch to media codec C2
    * Resource taken From Sunfish.
    
    Change-Id: I793e847bed6941cb22d6ac9aea60fa2e48f13068
    Signed-off-by: menorziin <williamsilvapppl@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -175,7 +175,9 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(VENDOR_PATH)/device_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(VENDOR_PATH)/manifest.xml
+DEVICE_MANIFEST_FILE :=  \
+    $(VENDOR_PATH)/manifest.xml \
+    hardware/qcom-caf/sm8150/media/conf_files/msmnile/c2_manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(VENDOR_PATH):libinit_sm6150
